492b826cd5b0eaadb52838e95b0ddc5c402b4788
Jake <jgevenson@gmail.com>
2024-02-16 01:10:09 -0600

    Add files via upload

---
+++ sandbox/app/page.tsx
@@ -0,0 +1,23 @@
+// pages/index.tsx
+import type { NextPage } from "next";
+import { Container, Typography, Button } from "@mui/material";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Container maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Welcome to the Kitchen Management System
+        </Typography>
+        <Typography variant="body1">
+          Manage your kitchen orders efficiently and seamlessly with our
+          integrated system.
+        </Typography>
+        <Link href={"/orderList"}>Click here</Link>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
